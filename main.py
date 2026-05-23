746287e30b849a22da026469977ee54ace92c1a5
jasperthefish <vrithikavaithi@gmail.com>
2026-05-23 13:36:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 # Create a random password generator that returns passwords composed of lowercase letters, uppercase letters, digits, and punctuations
 
 import tkinter as tk
+from tkinter import messagebox
 import random as r
 
-#command
-# def password_generator():
-#     for characters in range(r.randrange(8, 13)):
-#         print(characters)
+# commands
+def password_generator():
+        password = []
+        uppercase = ["A", "B", "C", "D", "E", "F", "G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        lowercase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+        numbers = ["0", "1","2","3","4","5","6","7","8","9","10",]
+        special = ["!","@","#","$","%","&"]
+        length = r.randint(8, 20)
+        for i in range(length):
+                character = r.randint(1,4)
+                if character == 1:
+                        password.append(uppercase[r.randint(0, len(uppercase)-1)])
+                elif character == 2:
+                        password.append(lowercase[r.randint(0, len(lowercase)-1)])
+                elif character == 3:
+                        password.append(numbers[r.randint(0, len(numbers)-1)])
+                else:
+                        password.append(special[r.randint(0, len(special)-1)])
+        print(password)
 
+def show():
+        messagebox.showinfo("password", password_generator())
 
-# # window parameters
-# window = tk.Tk()
-# window.title("Random Password Generator")
-# window.geometry("200x200")
+# window parameters
+window = tk.Tk()
+window.title("Random Password Generator")
+window.geometry("200x200")
 
-# # button
-# tk.Button(window, text = "Click to generate a random password!", width = 100, height = 100, command = password_generator).pack()
+# button
+tk.Button(window, text = "Click to generate a random password!", width = 100, height = 50, command = show).pack()
 
-
-
-# window.mainloop()
-
-for characters in range(8, r.randint(8, 13)):
-        print(characters)
+window.mainloop()
